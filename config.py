5fc048884e37731f4a5ec8732d02ff25d0241396
Abdo GA <137944826+itsAbdoGa@users.noreply.github.com>
2025-07-29 10:33:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,8 @@ class Config:
     SECRET_KEY = "admin"
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
-    DATABASE_DIR = os.path.join(BASE_DIR, "database")
-    UPCZIP_DB = os.path.join(DATABASE_DIR, "upczip.db")  # Full path
-    DATABASE = os.path.join(DATABASE_DIR, "stores.db")    # Full path
+    UPCZIP_DB = "/database/upczip.db"
+    DATABASE = "/database/stores.db"
     API_URL = "http://5.75.246.251:9099/stock/store"
     MAX_LOGS = 10
     MAX_RESULTS_IN_SESSION = 10
